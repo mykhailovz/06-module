96c861634a7bcea5d3b1bf4cbc74e348f0136a58
mykhailovozniaklv1 <mykhailovozniak@yahoo.com>
2023-09-27 10:30:27 +0200

    [use-search-params]: add query params

--- src/components/GenreSelect.jsx
+++ src/components/GenreSelect.jsx
@@ -12,8 +12,8 @@ function GenreSelect({genres, genre, onSelect}) {
     <ul id="genres-list" data-testid='genres-list' className="flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
       {genres.map(g => {
         return (
-          <li className={`${selectedGenre?.id === g.id ? 'bg-gray-400' : ''}`} key={g.id} onClick={() => handleClick(g)}>
-            <a className='mr-4 md:mr-6 uppercase underline' href='#'>{g.name}</a>
+          <li className={`${selectedGenre === g ? 'bg-gray-400' : ''}`} key={g} onClick={() => handleClick(g)}>
+            <a className='mr-4 md:mr-6 uppercase underline' href='#'>{g}</a>
           </li>
         )
       })}
